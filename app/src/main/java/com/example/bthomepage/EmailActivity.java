5e4c4c40b9eba4cb47b5210e1cae7c6ac3443c92
Ashley Cao <yuanc0000@gmail.com>
2023-09-23 16:07:42 -0400

    generate and save csv file

--- app/src/main/java/com/example/bthomepage/EmailActivity.java
+++ app/src/main/java/com/example/bthomepage/EmailActivity.java
@@ -63,8 +63,11 @@ public class EmailActivity extends AppCompatActivity {
     }
 }
  */
+
+
 package com.example.bthomepage;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -80,7 +83,15 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class EmailActivity extends AppCompatActivity {
     private static final String TAG = "EmailActivity"; // For logging
@@ -120,6 +131,11 @@ public class EmailActivity extends AppCompatActivity {
                             }
                         }
                     }
+                    String csvContent = generateCSVContent(documentSnapshot);
+                    System.out.println("create csvContent with len ="+csvContent.length());
+                    saveToCSVFile(csvContent);
+                    System.out.println("after call saveToCSVFile(csvContent);");
+
                 } else {
                     Log.d(TAG, "No data found for user.");
                 }
@@ -131,5 +147,87 @@ public class EmailActivity extends AppCompatActivity {
             }
         });
     }
+
+    /*
+    private String generateCSVContent(DocumentSnapshot documentSnapshot) {
+        StringBuilder csvBuilder = new StringBuilder();
+
+        // Add header with column titles (dates)
+        csvBuilder.append("Exercise Name,");
+        for (String date : documentSnapshot.getData().keySet()) {
+            csvBuilder.append(date).append(",");
+        }
+        csvBuilder.append("\n");
+
+        // Iterate through exercises and fill data
+        String firstDateKey = documentSnapshot.getData().keySet().iterator().next();
+        Map<String, Long> firstDateData = (Map<String, Long>) documentSnapshot.get(firstDateKey);
+        for (String exerciseName : firstDateData.keySet()) {
+            csvBuilder.append(exerciseName).append(",");
+            for (String date : documentSnapshot.getData().keySet()) {
+                Object dataObj = documentSnapshot.get(date);
+                if (dataObj instanceof Map) {
+                    Map<String, Long> dataMap = (Map<String, Long>) dataObj;
+                    csvBuilder.append(dataMap.get(exerciseName)).append(",");
+                }
+            }
+            csvBuilder.append("\n");
+        }
+
+        return csvBuilder.toString();
+    }*/
+    private String generateCSVContent(DocumentSnapshot documentSnapshot) {
+        StringBuilder csvBuilder = new StringBuilder();
+
+        // First, create a list of valid date keys and valid exercise names
+        List<String> validDates = new ArrayList<>();
+        Set<String> exerciseNames = new HashSet<>();
+        for (String key : documentSnapshot.getData().keySet()) {
+            if (key.matches("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}")) {
+                validDates.add(key);
+                Map<String, Long> dataMap = (Map<String, Long>) documentSnapshot.get(key);
+                exerciseNames.addAll(dataMap.keySet());
+            }
+        }
+
+        // Create the header row with exercise names
+        csvBuilder.append("Date,");
+        for (String exercise : exerciseNames) {
+            csvBuilder.append(exercise).append(",");
+        }
+        csvBuilder.append("\n");
+
+        // Iterate through each valid date and fill in the data
+        for (String date : validDates) {
+            csvBuilder.append(date).append(",");
+            Map<String, Long> dataMap = (Map<String, Long>) documentSnapshot.get(date);
+            for (String exercise : exerciseNames) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    csvBuilder.append(dataMap.getOrDefault(exercise, 0L)).append(",");
+                }
+            }
+            csvBuilder.append("\n");
+        }
+
+        return csvBuilder.toString();
+    }
+
+
+    private void saveToCSVFile(String csvContent) {
+        String fileName = "exercise_data.csv";
+        File file = new File(getExternalFilesDir(null), fileName);
+        try (FileOutputStream out = new FileOutputStream(file)) {
+            out.write(csvContent.getBytes());
+            Log.d(TAG, "Saved to " + file.getAbsolutePath());
+            System.out.println("Saved to " + file.getAbsolutePath());
+        } catch (IOException e) {
+            Log.e(TAG, "Error saving CSV file", e);
+            System.out.println("Error saving CSV file "+e);
+        }
+    }
+
+
+
+
 }
 

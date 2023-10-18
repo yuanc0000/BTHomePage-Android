018c3555d5ba5a4f05d117488850fc3d00d6763b
Alina <yinyikun0@163.com>
2023-10-18 17:10:59 -0400

    Have the month & year displayed and a date highlighted

--- app/src/main/java/com/example/bthomepage/MatCal.java
+++ app/src/main/java/com/example/bthomepage/MatCal.java
@@ -1,22 +1,24 @@
 package com.example.bthomepage;
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Locale;
 
 public class MatCal extends AppCompatActivity {
 
     private CompactCalendarView compactCalendarView;
     private HashSet<Long> loginDates;
+    private TextView monthYearTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +36,14 @@ public class MatCal extends AppCompatActivity {
         Event ev1 = new Event(Color.GREEN, calendar.getTimeInMillis(), "Logged in on this day");
         compactCalendarView.addEvent(ev1);
 
-        // Define the date you want to highlight (October 17, 2023)
+        // Highlight a specific date (e.g., October 17, 2023)
         Calendar highlightDate = Calendar.getInstance();
         highlightDate.set(2023, Calendar.OCTOBER, 17);
-
-        // Create an event for the highlight date and set the color
         Event highlightEvent = new Event(Color.BLUE, highlightDate.getTimeInMillis(), "Highlighted Date");
         compactCalendarView.addEvent(highlightEvent);
+
+        // Initialize the TextView for the month and year
+        monthYearTextView = findViewById(R.id.monthYearTextView);
 
         compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
             @Override
@@ -56,8 +59,12 @@ public class MatCal extends AppCompatActivity {
 
             @Override
             public void onMonthScroll(Date firstDayOfNewMonth) {
-                // You can set the title or do something when the month is changed if needed
-                Toast.makeText(MatCal.this, "Month changed: " + firstDayOfNewMonth, Toast.LENGTH_SHORT).show();
+                // Update the TextView with the current month and year
+                SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy", Locale.getDefault());
+                String monthYear = dateFormat.format(firstDayOfNewMonth);
+                monthYearTextView.setText(monthYear);
+
+                Toast.makeText(MatCal.this, "Month changed: " + monthYear, Toast.LENGTH_SHORT).show();
             }
         });
     }

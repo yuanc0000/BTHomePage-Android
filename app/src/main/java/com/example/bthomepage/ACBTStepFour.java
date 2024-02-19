98d0e5175fc7a1746d3920a6b92c9fd05b69ed45
Alina <yinyikun0@163.com>
2024-02-20 00:17:20 +0800

    Adjusted and recompiled the UI for the first three exercises

--- app/src/main/java/com/example/bthomepage/ACBTStepFour.java
+++ app/src/main/java/com/example/bthomepage/ACBTStepFour.java
@@ -6,17 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class ACBTStepThree extends AppCompatActivity {
-    //shows step one of acbt exercise
+public class ACBTStepFour extends AppCompatActivity {
+    //shows step 2 of acbt exericse
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_acbtstep_three);
+        setContentView(R.layout.activity_acbtstep_four);
     }
-    //button click leads to step 2
-    public void sendMessagestepfourACBT(View view) {
-        Intent intent = new Intent(this, ACBTStepFour.class);
+    //button click leads to step 3
+    public void sendMessagestepfiveACBT(View view) {
+        Intent intent = new Intent(this, ACBTStepFive.class);
         startActivity(intent);
     }
-}
 
+
+}
